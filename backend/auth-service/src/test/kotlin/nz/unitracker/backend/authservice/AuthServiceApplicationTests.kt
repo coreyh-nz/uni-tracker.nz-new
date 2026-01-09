a9cc93f662f09cde07e812af16d0c3c3710b2e64
Corey <cbh344@outlook.com>
2026-01-10 11:57:55 +1300

    chore(#1): initial auth service module

---
+++ backend/auth-service/src/test/kotlin/nz/unitracker/backend/authservice/AuthServiceApplicationTests.kt
@@ -0,0 +1,12 @@
+package nz.unitracker.backend.authservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+    @Suppress("EmptyFunctionBlock")
+    @Test
+    fun contextLoads() {
+    }
+}
